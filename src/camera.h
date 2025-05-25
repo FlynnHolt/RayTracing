e4833360197c1cce78d734f3ea89e52e166d37b5
Flynn Holthouse <89971651+FlynnHolt@users.noreply.github.com>
2025-05-25 12:50:17 +0800

    Chapter 9 - Diffuse material

--- src/camera.h
+++ src/camera.h
@@ -8,6 +8,7 @@ class camera {
     double aspect_ratio = 1.0; // Default Aspect ratio
     int image_width = 100; // Default image width (pixels)
     int samples_per_pixel = 10; // Count of random samples for each pixel
+    int max_depth = 10; // Max number of ray bounces.
   
     void render(const hittable& world) {
         initialise();
@@ -23,7 +24,7 @@ class camera {
 
                 for (int sample = 0; sample < samples_per_pixel; sample++) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
 
                 write_color(std::cout, pixel_samples_scale * pixel_color);
@@ -91,11 +92,20 @@ class camera {
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        // If the max depth is exceeding =, we don't gather any more light.
+        if (depth <= 0) {
+            return color(0, 0, 0);
+        }
+
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)) {
-            return (rec.normal + color(1,1,1)) / 2;
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            // Diffuse Material:
+            // 1. Ray bounces in random direction and returns 50% of the light.
+            // 2. Lambertian reflection and 50% of the light
+            vec3 new_direction = rec.normal + random_unit_vector(); //random_on_hemisphere(rec.normal);
+            return 0.5 * ray_color(ray(rec.p, new_direction), depth-1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
